8d4cb781333cb7871084aedfd0b93c3e82933904
Ilya Irmashev <ilya290397@gmail.com>
2023-11-23 22:58:11 +0400

    20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, categories, catalog_product
 
 # В корневом файле urls.py, который находится в директории с настройками проекта, необходимо
 # описать новый маршрут, но вместо контроллера указать специальную функцию include.
 # При этом в приложении catalog должен появиться файл urls.py,
 # и уже в нём можно описывать необходимые маршруты
+
+app_name = CatalogConfig.name
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('categories/', categories, name='categories'),
+    path('<int:pk>/catalog/', catalog_product, name='catalog_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Contacts, Product
+
+
 # Create your views here.
 
 # Контроллер == функция def_contacts()
 #
 def contacts(request):
+    context = {
+        'object_list': Contacts.objects.all(),
+        'title': 'Наши контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'{name} ({email}): {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Category.objects.all()[:4],
+        'title': 'SkyStore'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Наши товары'
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def catalog_product(request, pk):
+    catalog_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Наши товары {catalog_item.name}'
+    }
+    return render(request, 'catalog/product.html', context)
